72cfa60335d6c233fc18ec5ccc34244139ecdc80
marcus <rhcn258@gmail.com>
2023-09-19 00:01:31 +0900

    feat: 헤더 작업중

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,15 @@
 import { useTheme } from "next-themes";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { cn } from "~lib/utils";
 
 function Header() {
   const { setTheme, theme } = useTheme();
+  const router = useRouter();
 
+  console.log(router.pathname, router.asPath);
+
+  const isActive = true;
   return (
     <header className="sticky top-0 z-10 w-full border-b bg-background/95 backdrop-blur">
       <div className="container flex h-14 items-center justify-between">
@@ -12,7 +18,21 @@ function Header() {
             Next-log
           </Link>
           <nav className="flex gap-6 items-center font-medium text-sm">
-            <a hclassName=""></a>
+            <Link
+              href="/article"
+              className={cn(
+                "transition-colors hover:text-foreground/80 text-foreground/60",
+                isActive && "text-foreground"
+              )}
+            >
+              Article
+            </Link>
+            <Link
+              href="/resume"
+              className="transition-colors hover:text-foreground/80 text-foreground/60"
+            >
+              Resume
+            </Link>
           </nav>
         </div>
         <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
