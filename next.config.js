77ac271ff189b3c54b490081695d55dcc7ff6651
marcus <rhcn258@gmail.com>
2023-10-05 01:12:13 +0900

    feat: Header 구현 완료

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "md", "mdx"],
+  reactStrictMode: true,
+});
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,42 +1,17 @@
-import { useTheme } from "next-themes";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { cn } from "~lib/utils";
+import SearchCommand from "./searchCommand";
+import NavToggles from "./toggle";
+import PageNav from "./nav";
 
 function Header() {
-  const { setTheme, theme } = useTheme();
-  const router = useRouter();
-
-  console.log(router.pathname, router.asPath);
-
-  const isActive = true;
   return (
     <header className="sticky top-0 z-10 w-full border-b bg-background/95 backdrop-blur">
       <div className="container flex h-14 items-center justify-between">
         <div className="md:flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
-            Next-log
-          </Link>
-          <nav className="flex gap-6 items-center font-medium text-sm">
-            <Link
-              href="/article"
-              className={cn(
-                "transition-colors hover:text-foreground/80 text-foreground/60",
-                isActive && "text-foreground"
-              )}
-            >
-              Article
-            </Link>
-            <Link
-              href="/resume"
-              className="transition-colors hover:text-foreground/80 text-foreground/60"
-            >
-              Resume
-            </Link>
-          </nav>
+          <PageNav />
         </div>
-        <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-          Toggle
+        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+          <SearchCommand />
+          <NavToggles />
         </div>
       </div>
     </header>
