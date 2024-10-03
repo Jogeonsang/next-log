3709777242409cd197098aa673a6b12063cd9dda
marcus <rhcn258@gmail.com>
2024-10-04 00:52:45 +0900

    feat: og generate 추가

---
+++ app/api/og/[slug]/route.tsx
@@ -0,0 +1,97 @@
+import { NextRequest } from "next/server";
+import { ImageResponse } from "next/og";
+
+// Pretendard 폰트 URL
+const pretendardFontUrl =
+  "https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/woff2/Pretendard-Regular.woff2";
+
+// 폰트 파일을 로드하는 함수
+async function loadFont() {
+  const fontData = await fetch(pretendardFontUrl).then((res) =>
+    res.arrayBuffer()
+  );
+  return fontData;
+}
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const title = searchParams.get("title") || "Title";
+  const description = searchParams.get("description") || "Default Description";
+
+  const fontData = await loadFont();
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: "flex",
+          height: "100%",
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "space-between",
+          flexDirection: "column",
+          backgroundImage: "linear-gradient(110deg, #f5f5f4, #dbeafe)",
+          fontSize: 60,
+          letterSpacing: -2,
+          fontWeight: "700",
+          textAlign: "center",
+          position: "relative",
+          fontFamily: "Pretendard",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: 26,
+            left: 40,
+            display: "flex",
+            flexDirection: "column",
+            color: "#0a0a0a",
+            fontWeight: "700",
+          }}
+        >
+          <span style={{ fontSize: 19 }}>Geon</span>
+          <span style={{ fontSize: 12 }}>marcus-log.dev</span>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexWrap: "wrap",
+            padding: "0px 30px",
+            paddingTop: "80px",
+            margin: "0 0",
+            maxWidth: "1050px",
+            minWidth: "100%",
+            borderBottomLeftRadius: "18px",
+            textAlign: "left",
+            color: "#0a0a0a",
+            lineHeight: "1.4",
+            fontSize: 30,
+            fontWeight: 700,
+          }}
+        >
+          {title}
+        </div>
+        <div
+          style={{
+            color: "#0a0a0a",
+            fontWeight: 600,
+            left: 40,
+            bottom: 26,
+            textAlign: "left",
+            position: "absolute",
+            fontSize: 20,
+            lineHeight: "1.75rem",
+          }}
+        >
+          {description}
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -36,36 +36,73 @@ const PostPage = async ({ params }: { params: { slug: string } }) => {
   };
 
   return (
-    <article className="prose dark:prose-invert max-w-none prose-pre:rounded-[9px] my-16">
-      <div className="max-w-[1000px] m-auto text-center">
-        <h1>{post.metadata.title}</h1>
-        {post.metadata.thumbnail && (
-          <Image
-            src={`/posts/${post.slug}/${post.metadata.thumbnail}`}
-            alt="post_thumbnail"
-            className="rounded-[14px]"
-            sizes="100vw"
-            style={{ width: "100%", height: "auto" }}
-            width={0}
-            height={0}
-          />
-        )}
-        <p>
-          {post.metadata.category} | {fomattedDate(post.metadata.date)}
-        </p>
-      </div>
-      <div className="max-w-[800px] m-auto">
-        <div className="flex-col my-12">
-          <h3 dangerouslySetInnerHTML={{ __html: post.metadata.introTitle }} />
-          <span dangerouslySetInnerHTML={{ __html: post.metadata.introDesc }} />
+    <>
+      <article className="prose dark:prose-invert max-w-none prose-pre:rounded-[9px] my-16">
+        <div className="max-w-[1000px] m-auto text-center">
+          <h1>{post.metadata.title}</h1>
+          {post.metadata.thumbnail && (
+            <Image
+              src={`/posts/${post.slug}/${post.metadata.thumbnail}`}
+              alt="post_thumbnail"
+              className="rounded-[14px]"
+              sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+              width={0}
+              height={0}
+            />
+          )}
+          <p>
+            {post.metadata.category} | {fomattedDate(post.metadata.date)}
+          </p>
         </div>
-        <hr className="border-1 w-4/12 m-auto mb-20" />
-        <div dangerouslySetInnerHTML={{ __html: post.content }} />
-      </div>
-    </article>
+        <div className="max-w-[800px] m-auto">
+          <div className="flex-col my-12">
+            <h3
+              dangerouslySetInnerHTML={{ __html: post.metadata.introTitle }}
+            />
+            <span
+              dangerouslySetInnerHTML={{ __html: post.metadata.introDesc }}
+            />
+          </div>
+          <hr className="border-1 w-4/12 m-auto mb-20" />
+          <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        </div>
+      </article>
+    </>
   );
 };
 export default PostPage;
+
+type Props = {
+  params: { slug: string };
+};
+
+export async function generateMetadata({ params }: Props) {
+  const cookieStore = cookies();
+  const lang = cookieStore.get("lang")?.value || i18nConfig.defaultLocale;
+  const post = await getPost(params.slug, lang);
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:4000";
+
+  const ogImageUrl = `${siteUrl}/api/og/${
+    params.slug
+  }?title=${encodeURIComponent(
+    post.metadata.title
+  )}&description=${encodeURIComponent(post.metadata.description)}`;
+
+  return {
+    title: post.metadata.title,
+    description: post.metadata.introDesc,
+    authors: {
+      name: "Geon",
+      url: "https://marcus-log/resume",
+    },
+    openGraph: {
+      images: post.metadata.thumbnail || ogImageUrl,
+      title: post.metadata.title,
+      description: post.metadata.introDesc,
+    },
+  };
+}
 
 export const generateStaticParams = async () => {
   const posts = getAllPosts();
